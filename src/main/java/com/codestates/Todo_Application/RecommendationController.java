2bdbe3f0f2319536ba0df26c5b1dc50152518a89
Goomt <serosm93@gmail.com>
2022-08-22 16:06:23 +0900

    initial

---
+++ src/main/java/com/codestates/Todo_Application/RecommendationController.java
@@ -0,0 +1,12 @@
+package com.codestates.Todo_Application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application!";
+    }
+}
